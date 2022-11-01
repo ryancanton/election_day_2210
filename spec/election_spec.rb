3411ca8e5f482f1b196fbc659873a82027b576d4
Ryan Canton <ryancanton999@gmail.com>
2022-11-01 09:31:40 -0600

    Test: Add test for add_race method of class Election

--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -17,4 +17,16 @@ RSpec.describe Election do
       expect(election.races).to eq([])
     end
   end
+
+  describe '#add_race' do
+    it 'adds race obects to races roster' do
+      election = Election.new('2022')
+      race1 = Race.new("Virginia District 4 Representative")
+      race2 = Race.new("Texas Governor")
+      election.add_race(race1)
+      election.add_race(race2)
+
+      expect(election.races).to eq([race1, race2])
+    end
+  end
 end
