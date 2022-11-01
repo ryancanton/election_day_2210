6c3e768bebb2ea6aadb4aae2073c0168b31b5495
Ryan Canton <ryancanton999@gmail.com>
2022-11-01 09:20:15 -0600

    Test: Add test for register_candidate! method for class Race

--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -17,4 +17,13 @@ RSpec.describe Race do
     end
   end
 
+  describe '#register_candidate!' do
+    it 'adds candidate object to roster for race' do
+      race = Race.new("Texas Governor")
+      candidate1 = race.register_candidate!({name: "Diana D", party: :democrat})
+      candidate2 = race.register_candidate!({name: "Roberto R", party: :republican})
+
+      expect(race.candidates).to eq([candidate1, candidate2])
+    end
+  end
 end
