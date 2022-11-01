1f4796e80a95345b63da8fd0e90aada9e7d78b26
Ryan Canton <ryancanton999@gmail.com>
2022-11-01 09:51:46 -0600

    Feat: Add open? method to class Race

--- lib/race.rb
+++ lib/race.rb
@@ -6,11 +6,16 @@ class Race
   def initialize(office)
     @office = office
     @candidates = []
+    @open = true
   end
 
   def register_candidate!(candidate_info)
     new_candidate = Candidate.new(candidate_info)
     @candidates.push(new_candidate)
     new_candidate
+  end
+
+  def open?
+    @open
   end
 end
