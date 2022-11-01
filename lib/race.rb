df3f8d73cca07284857487c7f651b7fd703db48a
Ryan Canton <ryancanton999@gmail.com>
2022-11-01 09:22:15 -0600

    Feat: Add register_candidate! method to class Race

--- lib/race.rb
+++ lib/race.rb
@@ -7,4 +7,10 @@ class Race
     @office = office
     @candidates = []
   end
+
+  def register_candidate!(candidate_info)
+    new_candidate = Candidate.new(candidate_info)
+    @candidates.push(new_candidate)
+    new_candidate
+  end
 end
